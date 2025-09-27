f83dcff38bf02c970b9fad215ae427549088d14b
Annisa <annisafitriana38@gmail.com>
2025-09-27 16:10:12 +0700

    Revert "error register"
    This reverts commit 7b55eae63dc44d0effe39d6e4fd122795cec06db.

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,20 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.js',
+                'resources/js/music-manager.js'
+            ],
             refresh: true,
         }),
     ],
+    optimizeDeps: {
+        include: ['alpinejs', 'axios']
+    },
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
